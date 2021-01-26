5c8427d297766928433fe3ca7ad4ded92477a6ef
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-01-26 13:35:47 -0600

    remove unnecessary code

--- bot.js
+++ bot.js
@@ -133,8 +133,6 @@ commands = {	// all commands list below
 			var args = suffix.split(" ");
 			var name = args.shift();
 			var house = args.toString();
-			console.log("var args = " + args);
-			console.log("var name = " + name)
 			if(!name) {
 				msg.channel.send(Config.commandPrefix + "view " + this.usage + "\n" + this.description);
 			} else if (name == "ravenclaw") {
