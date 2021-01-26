95617d35193f866fabe33a7efa214fdc1a5ae3f4
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-01-26 06:56:19 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ try{
 	Config = require("./config.json");
 } catch(e){ //no config file, use defaults
 	Config.debug = false;
-	Config.commandPrefix = 'p! ';
+	Config.commandPrefix = 'p!';
 	try{
 		if(fs.lstatSync("./config.json").isFile()){ // open config file
 			console.log("WARNING: config.json found but we couldn't read it!\n" + e.stack); // corrupted config file
