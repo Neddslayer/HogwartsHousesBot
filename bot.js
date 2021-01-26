6fd78b105992241d1953f4c6c225ac31018b2248
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-01-26 15:48:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ try{
 }
 
 function writeToFile(file, fileName) {
-	fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+	fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
 	  if (err) return console.log(err);
 	  console.log(JSON.stringify(file));
 	});
