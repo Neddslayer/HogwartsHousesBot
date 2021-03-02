2113e3a180a9493056f9adf97052ca428f02e02c
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-03-02 05:29:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,11 @@ client.connect(uri, function(err, client) {
 		const db = client.db("Data");
                 const col = db.collection("points");
 		var o_id = new mongo.ObjectID("603bdfad75a1e563bf49c584");
-                var values = col.findOne({"_id": o_id});
+                var values = col.findOne({"_id": o_id}).count()
+    			.then(function(values) {
+      			console.log(values); // Use this to debug
+      			callback(values);
+    			});
 		console.log(values);
                 var ravenPoints = values.ravenclaw;
                 var hufflePoints = values.hufflepuff;
