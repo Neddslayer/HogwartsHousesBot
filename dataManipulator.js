71c90119f9eeb3c9ea5713e25029b83bb92e1e9c
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-03-02 13:10:25 -0600

    Update dataManipulator.js

--- dataManipulator.js
+++ dataManipulator.js
@@ -7,25 +7,25 @@ const uri = process.env.DB_URI;
 
 function modDB(house, amount) {
 	client.connect(uri, function(err, client) {
-        if (err) {
-            console.log('Unable to connect to the mongoDB server. Error:', err);
-        } else {
-            console.log("Connected to mongoDB server");
+        	if (err) {
+        	    console.log('Unable to connect to the mongoDB server. Error:', err);
+        	} else {
+        	    console.log("Connected to mongoDB server");
+		
+            	    // Select database
+        	    const db = client.db('Data');
 
-            // Select database
-            const db = client.db('Data');
+         	   // Get the documents collection
+         	   var coll = db.collection('points');
 
-            // Get the documents collection
-            var coll = db.collection('points');
-
-            //We have a cursor now with our find criteria
-            var cursor = coll.find({
-                "query": "result"
-            });
-	    cursor.update({ $inc: { house: toInt32(amount) } });
-        }
-	modDB();
+         	   //We have a cursor now with our find criteria
+         	   var cursor = coll.find({
+         	       "query": "result"
+         	   });
+	  	  cursor.update({ $inc: { house: toInt32(amount) } });
+        	}
         // Close connection when done
         client.close();
-    });
+     });
+     modDB(house, amount);
 }
