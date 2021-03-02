95b96bdf81fffa43d2e2031b304206999a760f8b
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-03-02 12:35:51 -0600

    Create dataManipulator.js

---
+++ dataManipulator.js
@@ -0,0 +1,31 @@
+const mongo = require('mongodb');
+const client = mongo.MongoClient;
+const info = "[Bot/INFO] "
+const warn = "[Bot/WARN] "
+//no more hacking for you silly boi
+const uri = process.env.DB_URI;
+
+function modDB(house, amount) {
+	client.connect(uri, function(err, client) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            console.log("Connected to mongoDB server");
+
+            // Select database
+            const db = client.db('Data');
+
+            // Get the documents collection
+            var coll = db.collection('points');
+
+            //We have a cursor now with our find criteria
+            var cursor = coll.find({
+                "query": "result"
+            });
+	    cursor.update({ $inc: { house: toInt32(amount) } });
+            
+        }
+        // Close connection when done
+        client.close();
+    });
+}
