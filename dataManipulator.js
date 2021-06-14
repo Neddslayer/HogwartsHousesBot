727e30dd3f085e8592d590de2f19848198254224
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-06-14 18:51:47 -0500

    Update dataManipulator.js

--- dataManipulator.js
+++ dataManipulator.js
@@ -5,49 +5,6 @@ var db;
 var collection;
 var updateVal = {};
 
-function addPoints(house, amount) {
-	client.connect(uri, function(err, client) {
-		if(err){console.log('[Bot/WARN] Unable to connect to the mongoDB server. Error:', err);}
-        	
-        	console.log("[Server/INFO] Connected to mongoDB server to update value");
-		
-		db = client.db('Data');
-		var houseArray;
-		var newAmount;
-		collection = db.collection('points');
-		function getFields(input, field) {
-    			var output = [];
-   			for (var i=0; i < input.length ; ++i)
-       				output.push(input[i][field]);
-  			return output;
-		}
-		db.collection('points', function(err, collection) {
-    			collection.find({}).toArray(function(err, results) {
-        			houseArray = results;
-        			console.log(houseArray);
-    			});
-		});
-		async function newAmountAsync(house){
-			var array = await houseArray
-			var result;
-			try {
-				result = parseInt(getFields(array, house)) + parseInt(amount);
-			} catch(e) {
-				result = parseInt(getFields(array, 'ravenclaw')) + parseInt(amount);
-			}
-			updateVal[house] = newAmount;
-		}
-		
-		newAmountAsync(house);
-		
- 		console.log(updateVal);
-			
-	  	collection.updateOne({ "query" : "result" }, { $set: updateVal});
-		console.log("[Server/INFO] Successfully updated 1 document");
-        	
-	})
-}
-
 function modRaven(amount) {
 	client.connect(uri, function(err, client) {
         	if (err) {
@@ -128,4 +85,4 @@ function modGryffin(amount) {
         client.close();
      });
 }
-module.exports = { modRaven, modHuffle, modSlyther, modGryffin, addPoints };
+module.exports = { modRaven, modHuffle, modSlyther, modGryffin };
