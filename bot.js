d5542ca3fd58b4387547f69cd7c9d3d14794b9ed
Neddslayer <59270607+Neddslayer@users.noreply.github.com>
2021-06-14 19:01:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,7 +97,7 @@ commands = {	// all commands list below
         description: "Responds pong; useful for checking if bot is alive.",
         process: function(bot, msg, suffix) {
 	    var responseTime = Date.now() - msg.createdTimestamp;
-            msg.channel.send("Hippity hoppity your IP is now my property after " + responseTime.toString() + "ms");
+            msg.channel.send(":ping_pong:! Response time: " + responseTime.toString() + "ms");
             if(suffix){
                 msg.channel.send( "Note that p!ping takes no arguments!");
             }
